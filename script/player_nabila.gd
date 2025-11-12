055eb3fa3d57ce54cbc3746eb86ba4d8ec580588
Yssrrr20 <muhyassar20@gmail.com>
2025-11-13 01:57:15 +0700

    add coin, mob, more player movement

--- script/player_nabila.gd
+++ script/player_nabila.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -250.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,12 +12,25 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Get input direction : -1, 0, 1
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# flip character
+	if direction > 0 :
+		animated_sprite.flip_h = false
+	elif direction < 0 :
+		animated_sprite.flip_h = true
+	
+	# play animation
+	if direction == 0:
+		animated_sprite.play("Idle")
+	else:
+		animated_sprite.play("Run")
+	
+	# aply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:

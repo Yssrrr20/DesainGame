8fa7f89736cde7820202d043b73e9bfd1d4841af
Yssrrr20 <muhyassar20@gmail.com>
2025-11-27 02:37:14 +0700

    expand map

--- script/mob.gd
+++ script/mob.gd
@@ -1,19 +1,36 @@
 extends Node2D
 
-const  SPEED = 60
-var direction = 1
+const SPEED = 60
+var direction = 1 # 1 = Kanan, -1 = Kiri
+
+# Atur jangkauan pergerakan 
+@export var movement_range_x: float = 100.0 
+
+var start_x: float = 0.0 # Posisi X awal mob
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var ray_cast_right: RayCast2D = $ray_cast_right
-@onready var ray_cast_left: RayCast2D = $ray_cast_left
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _ready():
+	# Simpan posisi X awal
+	start_x = position.x
+
+
 func _process(delta):
-	if ray_cast_right.is_colliding():
-		direction = -1
-		animated_sprite.flip_h = true
-	if ray_cast_left.is_colliding():
-		direction = 1
-		animated_sprite.flip_h = false
-	position.x += direction * SPEED * delta
+	# Hitung batas minimum dan maksimum
+	var min_x = start_x - movement_range_x
+	var max_x = start_x + movement_range_x
 	
+	# Logika Pembalikan Arah
+	if direction == 1: # Bergerak ke Kanan
+		if position.x >= max_x:
+			direction = -1 # Balik ke Kiri
+			animated_sprite.flip_h = true
+	
+	elif direction == -1: # Bergerak ke Kiri
+		if position.x <= min_x:
+			direction = 1 # Balik ke Kanan
+			animated_sprite.flip_h = false
+			
+	# Gerakkan Mob
+	position.x += direction * SPEED * delta

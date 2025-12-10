6514bdeaf3c8f314b212cfa84391a78b9ceb9fbc
Yssrrr20 <muhyassar20@gmail.com>
2025-12-11 04:06:10 +0700

    Add music BG & SFX for pickup util

--- scripts/collectible.gd
+++ scripts/collectible.gd
@@ -1,7 +1,8 @@
 extends Area2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_area_entered(area: Area2D) -> void:
-	queue_free()
+	animation_player.play("pickUp animation")
 	var body = area.get_parent();
 	if body.has_method("add_score"):
 		body.add_score(1)
